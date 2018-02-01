2837be5f39e5084d9139607a8d0d7988f676b4d4
tomyuan520 <tomyuan520@github.com>
2018-01-31 21:47:16 -0500

    mostly working ver.
    part 1 finished at 80%

--- part1.py
+++ part1.py
@@ -13,25 +13,29 @@ g = 9.8
 t = d/vX
 #t is the time that bird needs to reach the building.
 
-tdec = vY/g
+tDec = vY/g
 #tdec is the time that the bird fly up.
 
 hhit = 5+vY*t-0.5*g*t**2
 #hhit is the height when the bird (maybe) touches the building.
 
+vF = math.sqrt(vX**2 + (g*(t-tDec))**2)
+rad = math.atan((g*(t-tDec))/vX)
+arc = rad/math.pi*180
+#vF is the final velocity of the bird. arc is the angular of the speed.
 
-
-
+print('\nthe bird is travelling at a speed of ' + str(vF) + ' meters per second.\n')
+print('and the speed is at an angle of ' + str(arc) + ' degrees below the horizontal.\n')
 
 if hhit > 0 and hhit < h:
-    print("\nthe bird reaches the building in " + str(t) + ' seconds.\n' )
+    print("the bird reaches the building in " + str(t) + ' seconds.\n' )
     print("the bird is at a height of " + str(hhit) + " when it reaches the building.\n" )
-    print("so it hits the building")
+    print("so it hits the building.\n")
 elif hhit > 0 and hhit > h:
     print("\nthe bird is at a height of " + str(hhit) + " when it reaches the building.\n" )
-    print("so it flies over the building.")
+    print("so it flies over the building.\n")
 elif hhit < 0:
     print("\nthe bird stops before it reaches the building.\n" )
-    print("so it won't hit the building.")
+    print("so it won't hit the building.\n")
 
 
