744d83e5eefa05c9075828e5ae474ca361f9fb12
tomyuan520 <tomyuan520@github.com>
2018-01-31 21:19:04 -0500

    mending ver.1

--- part1.py
+++ part1.py
@@ -8,23 +8,29 @@ h = int(input('The height of the structure in m:' ))
 t = d/(v*math.cos(a/180*math.pi))
 #t is the time that bird needs to reach the building.
 
-tdec = (v*math.sin(a/180*math.pi))/9.8  
+tdec = (v*math.sin(a/180*math.pi))/9.8
 #tdec is the time that the bird fly up.
 
-hhit = 0.5*9.8*(t-tdec)*(t-tdec) 
+hhit = 5+(v*math.sin(a/180*math.pi))*t-0.5*9.8*t*t
 #hhit is the height when the bird (maybe) touches the building.
 
 
 
 if hhit > 0 and hhit < h:
-    print("the bird reaches the building in " + str(t) + ' seconds')
-    print("the bird is at a height of " + str(hhit) + " when it reaches the building")
+    print("\nthe bird reaches the building in " + str(t) + ' seconds.\n' )
+    
+    print("the bird is at a height of " + str(hhit) + " when it reaches the building.\n" )
+    
     print("so it hits the building")
 elif hhit > 0 and hhit > h:
-    print("the bird is at a height of " + str(hhit) + " when it reaches the building")
-    print("so it flies over the building")
+    
+    print("\nthe bird is at a height of " + str(hhit) + " when it reaches the building.\n" )
+    
+    print("so it flies over the building.")
 elif hhit < 0:
-    print("the bird stops before it reaches the building")
-    print("so it won't hit the building")
+    
+    print("\nthe bird stops before it reaches the building.\n" )
+    
+    print("so it won't hit the building.")
 
 
